9cc70f79c232ab952772ed0f9f1e4f275f64f88b
lesaltzm <lesaltzm@microsoft.com>
2020-09-15 16:35:41 -0700

    Don't exclude false literals from checksum

--- src/PAModelTests/ChecksumTests.cs
+++ src/PAModelTests/ChecksumTests.cs
@@ -12,7 +12,7 @@ namespace PAModelTests
     public class ChecksumTests
     {
         [DataTestMethod]
-        [DataRow("MyWeather.msapp", "C1_0NR4SJOpBD7nc5k/6JoBp0822aEFJyOBo9a7AOvr/Qw=")]
+        [DataRow("MyWeather.msapp", "C1_TJ+ZAELmkaG96tYuftn6+qKDyQfo0xga/KKmAXcZ/ek=")]
         public void TestChecksum(string filename, string expectedChecksum)
         {
             var root = Path.Combine(Environment.CurrentDirectory, "Apps", filename);
