3b6d36ec2e7944953b4b7f42d77e10cce605768a
lesaltzm <lesaltzm@microsoft.com>
2020-10-14 14:22:48 -0700

    Add Gallery Test App and .pa

--- src/PAModelTests/SourceDecoderTests.cs
+++ src/PAModelTests/SourceDecoderTests.cs
@@ -15,12 +15,13 @@ namespace PAModelTests
         // Compare actual source output. This catches things like:
         // - are we removing default properties, from both Theme Json and Template xmL?
         // - canonical ordering and stable output 
-        [TestMethod]
-        public void TestMethod1()
+        [DataTestMethod]
+        [DataRow("MyWeather.msapp", "Weather_Screen1.pa1")]
+        [DataRow("GalleryTestApp.msapp", "Gallery_ScreenTest.pa1")]
+        public void TestScreenBaselines(string appName, string screenBaselineName)
         {
             // Pull both the msapp and the baseline from our embedded resources. 
-            var filename = "MyWeather.msapp";
-            var root = Path.Combine(Environment.CurrentDirectory, "Apps", filename);
+            var root = Path.Combine(Environment.CurrentDirectory, "Apps", appName);
             Assert.IsTrue(File.Exists(root));
 
             var msapp = CanvasDocument.LoadFromMsapp(root);
@@ -32,7 +33,7 @@ namespace PAModelTests
                 msapp.SaveAsSource(outSrcDir);
 
                 var pathActual = Path.Combine(outSrcDir, "Src", "Screen1.pa1");
-                var pathExpected = Path.Combine(Environment.CurrentDirectory, "SrcBaseline", "Weather_Screen1.pa1");
+                var pathExpected = Path.Combine(Environment.CurrentDirectory, "SrcBaseline", screenBaselineName);
 
 
                 // TODO - as the format stabalizes, we can compare more aggressively.
