012ff227adfd2b5a2858abd8d4a5d6082d9f5e33
lesaltzm <lesaltzm@microsoft.com>
2020-12-15 16:07:27 -0800

    Cleanup

--- src/PAModel/Entropy.cs
+++ src/PAModel/Entropy.cs
@@ -35,9 +35,6 @@ namespace Microsoft.PowerPlatform.Formulas.Tools
         // Key is component name, value is Index. 
         public Dictionary<string, double> ComponentIndexes { get; set; } = new Dictionary<string, double>();
 
-        // Per control file, stores the publish order index offset to that file
-        public Dictionary<string, int> PublishOrderIndexOffsets { get; set; } = new Dictionary<string, int>();
-
         public int GetOrder(DataSourceEntry dataSource)
         {
             return this.OrderDataSource.GetOrDefault<string,int>(dataSource.GetUniqueName(), -1);
