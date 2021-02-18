7713b0ba5bad14d8bb3fe9e40f96e8d1b7c54549
lesaltzm <lesaltzm@microsoft.com>
2021-02-18 15:30:37 -0800

    foo

--- src/PAModel/MergeTool/Deltas/AddControl.cs
+++ src/PAModel/MergeTool/Deltas/AddControl.cs
@@ -1,3 +1,5 @@
+using Microsoft.PowerPlatform.Formulas.Tools.EditorState;
+using Microsoft.PowerPlatform.Formulas.Tools.IR;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -6,5 +8,8 @@ namespace Microsoft.PowerPlatform.Formulas.Tools.MergeTool.Deltas
 {
     internal class AddControl
     {
+        public ControlPath ParentControlPath;
+        public BlockNode Control;
+        public ControlState ControlState;
     }
 }
