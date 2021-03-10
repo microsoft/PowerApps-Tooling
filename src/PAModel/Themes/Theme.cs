8140416a16fa9a36e957c9f1cc348101ef5fea55
lesaltzm <lesaltzm@microsoft.com>
2021-03-10 10:42:23 -0800

    theme start

--- src/PAModel/Themes/Theme.cs
+++ src/PAModel/Themes/Theme.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Diagnostics.Contracts;
+using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -18,54 +19,71 @@ namespace Microsoft.PowerPlatform.Formulas.Tools
         // Outer key is stylename, inner key is property name, inner value is expression
         private readonly Dictionary<string, Dictionary<string, string>> _styles = new Dictionary<string, Dictionary<string, string>>();
         
-        public Theme(ThemesJson themeJson)
+        public Theme(ThemesJson themeJson, ErrorContainer errors)
         {
             Contract.Assert(themeJson != null);
-            
+
+            var currentThemeName = themeJson.CurrentTheme;
+            bool loadedTheme = false;
+
+            var namedThemes = themeJson.CustomThemes.ToDictionary(theme => theme.name);
+            // First try to load the named theme
             foreach (var theme in themeJson.CustomThemes)
             {
-                Dictionary<string, string> palleteRules = new Dictionary<string, string>();
-                foreach(var item in theme.palette)
-                {
-                    palleteRules[item.name] = item.value;
-                }
+                if (theme.name != currentThemeName)
+                    continue;
+                loadedTheme = true;                
+            }
 
-                foreach (var style in theme.styles)
-                {
-                    var styleName = style.name;
-                    foreach (var prop in style.propertyValuesMap)
-                    {
-                        var propName = prop.property;
-                        var ruleValue = prop.value;
-                                                
-                        // TODO - share with logic in D:\dev\pa2\PowerApps-Client\src\Cloud\DocumentServer.Core\Document\Document\Theme\ControlStyle.cs
-                        // Resolve %%, from palette.
-                        {
-                            var match = Regex.Match(ruleValue, "%Palette.([^%]+)%");
-                            if (match.Success)
-                            {
-                                var group = match.Groups[1];
-
-
-
-                                string resourceValue;
-                                // Template may refer to a missing rule. 
-                                if (palleteRules.TryGetValue(group.ToString(), out resourceValue))
-                                {
-                                    ruleValue = ruleValue.Replace(match.Value, resourceValue);
-                                }
-                            }
-                        }
-
-                        ruleValue = ControlTemplateParser.UnescapeReservedName(ruleValue);
-
-                        _styles.GetOrCreate(styleName).Add(propName, ruleValue);
-                    }
-                }
+            if (!loadedTheme)
+            {
+                errors.ValidationError($"No themes matching theme name \"{currentThemeName}\" or \"defaultTheme\" were found in Themes.json");
+                throw new DocumentException();
             }
         }
 
-        
+
+        private void LoadTheme(CustomThemeJson theme)
+        {
+            Dictionary<string, string> palleteRules = new Dictionary<string, string>();
+            foreach (var item in theme.palette)
+            {
+                palleteRules[item.name] = item.value;
+            }
+
+            foreach (var style in theme.styles)
+            {
+                var styleName = style.name;
+                foreach (var prop in style.propertyValuesMap)
+                {
+                    var propName = prop.property;
+                    var ruleValue = prop.value;
+
+                    // TODO - share with logic in D:\dev\pa2\PowerApps-Client\src\Cloud\DocumentServer.Core\Document\Document\Theme\ControlStyle.cs
+                    // Resolve %%, from palette.
+                    {
+                        var match = Regex.Match(ruleValue, "%Palette.([^%]+)%");
+                        if (match.Success)
+                        {
+                            var group = match.Groups[1];
+
+
+
+                            string resourceValue;
+                            // Template may refer to a missing rule. 
+                            if (palleteRules.TryGetValue(group.ToString(), out resourceValue))
+                            {
+                                ruleValue = ruleValue.Replace(match.Value, resourceValue);
+                            }
+                        }
+                    }
+
+                    ruleValue = ControlTemplateParser.UnescapeReservedName(ruleValue);
+
+                    _styles.GetOrCreate(styleName).Add(propName, ruleValue);
+                }
+            }
+        }
 
         public bool TryLookup(string styleName, string propertyName, out string defaultScript)
         {
