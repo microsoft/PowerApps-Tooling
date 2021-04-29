15c736ab4553d84fcda435d8cb848e066b0e0147
Steven Rasmussen <steven.rasmussen@gmail.com>
2021-04-29 08:28:54 -0600

    Updated UtilityTests.TestRelativePath tests.

--- src/PAModelTests/UtilityTests.cs
+++ src/PAModelTests/UtilityTests.cs
@@ -3,6 +3,7 @@
 
 using Microsoft.PowerPlatform.Formulas.Tools;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.IO;
 
 namespace PAModelTests
@@ -36,18 +37,24 @@ namespace PAModelTests
         }
 
         [DataTestMethod]
-        // [DataRow("C:\\Foo\\Bar\\Baz", "C:\\Foo", "Bar\\Baz\\")]
-        // [DataRow("C:\\Foo\\Bar\\Baz", "C:\\Foo\\", "Bar\\Baz\\")]
-        // [DataRow("C:\\Foo\\Bar\\Baz\\", "C:\\Foo\\", "Bar\\Baz\\")]
-        // [DataRow("C:\\Foo\\Bar.msapp", "C:\\Foo", "Bar.msapp")]
-        // [DataRow("C:\\Foo\\Bar.msapp", "C:\\Foo\\", "Bar.msapp")]
-        // [DataRow("C:\\Foo\\Bar.msapp", "C:\\", "Foo\\Bar.msapp")]
-        [DataRow(@"C:\DataSources\JourneyPlanner|Sendforapproval.json", "C:\\", @"DataSources\JourneyPlanner|Sendforapproval.json")]
-        [DataRow(@"C:\DataSources\JourneyPlanner%7cSendforapproval.json", "C:\\", @"DataSources\JourneyPlanner%7cSendforapproval.json")]
-        // [DataRow(@"d:\app\Src\EditorState\Screen%252.editorstate.json",
-        //     @"d:\app", @"Src\EditorState\Screen%252.editorstate.json" )]
+        [DataRow(@"C:\Temp\DataSources\JourneyPlannerSendforapproval.json", @"C:\Temp", @"DataSources\JourneyPlannerSendforapproval.json")]
+        [DataRow(@"C:\Temp\DataSources\JourneyPlanner%7cSendforapproval.json", @"C:\Temp", @"DataSources\JourneyPlanner%7cSendforapproval.json")]
+        [DataRow(@"C:\Temp\MySolution\MySolution.Project\DataSources\JourneyPlanner%7cSendforapproval.json", @"C:\Temp\MySolution\MySolution.Project", @"DataSources\JourneyPlanner%7cSendforapproval.json")]
         public void TestRelativePath(string fullPath, string basePath, string expectedRelativePath)
         {
+            // The 'Utilities.AppendDirectorySeparatorChar' method (which the 'Utilities.GetRelativePath' depends on)
+            // has some logic that is better able to determine if a '\' is necessary for a directory based on the existence of the file/directory.
+            // So we need to create the file here for the updated logic to work.
+            // If the file/directory is not created then the logic falls back to the default logic
+
+            if (!File.Exists(fullPath))
+            {
+                var directory = Path.GetDirectoryName(fullPath);
+                if (!Directory.Exists(directory))
+                    Directory.CreateDirectory(directory);
+                File.Create(fullPath);
+            }
+
             // Test non-windows paths if on other platforms
             if (Path.DirectorySeparatorChar != '\\')
             {
@@ -55,9 +62,9 @@ namespace PAModelTests
                 basePath = basePath.Replace('\\', '/');
                 expectedRelativePath = expectedRelativePath.Replace('\\', '/');
             }
+
             Assert.AreEqual(expectedRelativePath, Utilities.GetRelativePath(fullPath, basePath));
         }
-
 
         // Verify regression from
         // https://github.com/microsoft/PowerApps-Language-Tooling/issues/153
