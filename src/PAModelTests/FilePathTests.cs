e593c9769cb1fdeee088adb31fe605569a7c11ac
Sushil Kumar <suskumar@microsoft.com>
2021-06-02 11:40:18 -0700

    Added more assertion

--- src/PAModelTests/FilePathTests.cs
+++ src/PAModelTests/FilePathTests.cs
@@ -13,14 +13,15 @@ namespace PAModelTests
     {
         // If the length of the escaped file name is beyond 60, then we truncate the length to limit it to 60.
         [DataTestMethod]
-        [DataRow("Long*Name*File*Path*Validation*Tests***.fx.yaml", FilePath.MaxFileNameLength)]
-        [DataRow("TestFileName.fx.yaml", 20)]
-        [DataRow("одиндвао", 8)] // test with unicode characters.
-        public void TestFileNames(string path, int expectedLength)
+        [DataRow("Long*Name*File*Path*Validation*Tests***.fx.yaml", FilePath.MaxFileNameLength, "Long%2aName%2aFile%2aPath%2aValidation%2aTests%2a109.fx.yaml")]
+        [DataRow("TestFileName.fx.yaml", 20, "TestFileName.fx.yaml")]
+        [DataRow("одиндвао", 8, "одиндвао")] // test with unicode characters.
+        public void TestFileNames(string path, int expectedLength, string expectedName)
         {
             var file = new FilePath(path);
             var str = file.ToPlatformPath();
             Assert.AreEqual(str.Length, expectedLength);
+            Assert.AreEqual(str, expectedName);
         }
     }
 }
