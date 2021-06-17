8885a526f930b83cb07342142b40083a672bc576
Sushil Kumar <suskumar@microsoft.com>
2021-06-17 16:28:47 -0700

    Updaing SrcBaseline tests

--- src/PAModelTests/SourceDecoderTests.cs
+++ src/PAModelTests/SourceDecoderTests.cs
@@ -17,11 +17,12 @@ namespace PAModelTests
         // - canonical ordering and stable output 
         [DataTestMethod]
         [DataRow("MyWeather.msapp", "", "Screen1.fx.yaml", "Weather_Screen1.fx.yaml")]
-        [DataRow("GroupControlTest.msapp", "",  "Screen1.fx.yaml", "GroupControl_Test.fx.yaml")]
-        [DataRow("GalleryTestApp.msapp", "",  "Screen1.fx.yaml", "Gallery_ScreenTest.fx.yaml")]
+        [DataRow("GroupControlTest.msapp", "", "Screen1.fx.yaml", "GroupControl_Test.fx.yaml")]
+        [DataRow("GalleryTestApp.msapp", "", "Screen1.fx.yaml", "Gallery_ScreenTest.fx.yaml")]
         [DataRow("SimpleScopeVariables.msapp", "Components", "Component1.fx.yaml", "ComponentFunction_Test.fx.yaml")]
-        [DataRow("TestStudio_Test.msapp", "Tests", "Suite.fx.yaml", "TestStudio_Test.fx.yaml")]
-        [DataRow("autolayouttest.msapp", "",  "Screen1.fx.yaml", "AutoLayout_test.fx.yaml")]
+        [DataRow("TestStudio_Test.msapp", "Tests", "Suite.fx.yaml", "TestStudio_Test_Suite.fx.yaml")]
+        [DataRow("TestStudio_Test.msapp", "Tests", "Test_7F478737223C4B69.fx.yaml", "TestStudio_Test.fx.yaml")]
+        [DataRow("autolayouttest.msapp", "", "Screen1.fx.yaml", "AutoLayout_test.fx.yaml")]
         public void TestScreenBaselines(string appName, string basePath, string sourceFileName, string screenBaselineName)
         {
             // Pull both the msapp and the baseline from our embedded resources. 
