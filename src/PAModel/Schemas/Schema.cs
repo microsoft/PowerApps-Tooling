bde1b7383a617b9816a86b678d2004cfc6149f0f
Mike Stall <jmstall@microsoft.com>
2022-02-03 11:23:37 -0800

    Pull in YamlDotNet. Generic serializer for Yaml pocos,

---
+++ src/PAModel/Schemas/Schema.cs
@@ -0,0 +1,56 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.PowerPlatform.Formulas.Tools.Schemas
+{
+    // Define Schema.yaml - strong typing for input and output parameters
+    public class ParameterSchema
+    {
+        // What names here?
+        public enum ParamType
+        {
+            Number,
+            String,
+            Boolean,
+            Date,
+            Time,
+            DateTime,
+            Color,
+            Guid,
+            Record,
+            Table,
+            EntityRecord,
+            EntityTable,
+        }
+
+        public class Parameter
+        {
+            public ParamType Type { get; set; }
+            public bool Required { get; set; }
+
+            // $$$ Default Value?
+
+            // Optional: For entities, specifies the name
+            public string Name { get; set; }
+
+            // Optional: For records, can become recursive
+            public Dictionary<string, Parameter> Fields { get; set; }
+        }
+
+        // $$$ enforce Case insenstive
+
+
+        // Input parameters (public)
+        public Dictionary<string, Parameter> Parameters { get; set; }
+
+        // Output parameters (public)
+        public Dictionary<string, Parameter> Outputs { get; set; }
+
+        // Global variables within this page.  (internal)
+        public Dictionary<string, Parameter> Globals { get; set; }
+    }
+}
