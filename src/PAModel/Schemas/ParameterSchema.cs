a57cb157a23b6da15a42ed0cdff21827b6f4c573
McCall Saltzman <69215460+lesaltzm@users.noreply.github.com>
2022-02-28 10:35:48 -0800

    Add CDS specific table name handling to ParameterSchema (#352)
    * Add CDS special handling to ParameterSchema
    
    * Updated schema

--- src/PAModel/Schemas/ParameterSchema.cs
+++ src/PAModel/Schemas/ParameterSchema.cs
@@ -29,15 +29,17 @@ namespace Microsoft.PowerPlatform.Formulas.Tools.Schemas
         public class Parameter
         {
             public ParamType Type { get; set; }
+
             public bool Required { get; set; }
 
-            // Optional: For entities, specifies the name
-            public string Name { get; set; }
+            // Optional: For EntityRecord and EntityTable, specifies the logical name of the table
+            // that the type is derived from
+            public string TableLogicalName { get; set; }
 
             // Optional: For records, can become recursive
             public Dictionary<string, Parameter> Fields { get; set; }
         }
-                
+
         // Input parameters (public)
         public Dictionary<string, Parameter> Parameters { get; set; }
 
