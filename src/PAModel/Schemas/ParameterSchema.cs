907c9cf4b4828237afa84ec8b1603ba8dcb6e97f
McCall Saltzman <69215460+lesaltzm@users.noreply.github.com>
2022-03-15 11:58:51 -0700

    Bring parameterschema in line with PowerFx FormulaTypeSchema (#361)

--- src/PAModel/Schemas/ParameterSchema.cs
+++ src/PAModel/Schemas/ParameterSchema.cs
@@ -18,26 +18,46 @@ namespace Microsoft.PowerPlatform.Formulas.Tools.Schemas
             Date,
             Time,
             DateTime,
+            DateTimeNoTimeZone,
             Color,
             Guid,
             Record,
             Table,
+            Blank,
+            Hyperlink,
+            OptionSetValue,
+            UntypedObject,
             EntityRecord,
             EntityTable,
         }
 
+        // Keep in sync with and eventually depend on Power-Fx/src/libraries/Microsoft.PowerFx.Core/Public/Types/Serialization/FormulaTypeSchema.cs
         public class Parameter
         {
+            /// <summary>
+            /// Represents the type of this item. For some complex types, additional optional data is required.
+            /// </summary>
             public ParamType Type { get; set; }
 
-            public bool Required { get; set; }
+            /// <summary>
+            /// Optional. For Records and Tables, contains the list of fields.
+            /// </summary>
+            public Dictionary<string, Parameter> Fields { get; set; }
 
-            // Optional: For EntityRecord and EntityTable, specifies the logical name of the table
-            // that the type is derived from
+            /// <summary>
+            /// Optional. Used for external schema definitions and input validation.
+            /// </summary>
+            public bool? Required { get; set; }
+
+            /// <summary>
+            /// Optional. For entities, specifies the table logical name.
+            /// </summary>
             public string TableLogicalName { get; set; }
 
-            // Optional: For records, can become recursive
-            public Dictionary<string, Parameter> Fields { get; set; }
+            /// <summary>
+            /// Optional. For Option Set Values, specifies the option set logical name.
+            /// </summary>
+            public string OptionSetName { get; set; }
         }
 
         // Input parameters (public)
