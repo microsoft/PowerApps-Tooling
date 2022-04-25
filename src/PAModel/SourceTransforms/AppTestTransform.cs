8d10f044304b13ade8ca2cf7201f9f9a386f2082
Landon Rogers <landonrogers@microsoft.com>
2022-04-25 17:13:20 -0400

    test rm return undo

--- src/PAModel/SourceTransforms/AppTestTransform.cs
+++ src/PAModel/SourceTransforms/AppTestTransform.cs
@@ -138,7 +138,11 @@ namespace Microsoft.PowerPlatform.Formulas.Tools.SourceTransforms
 
         public void BeforeWrite(BlockNode control)
         {
-
+            // If TestStepsMetadata does not exist, return early
+            if (!_entropy.DoesTestStepsMetadataExist)
+            {
+                return;
+            }
 
             var testStepsMetadata = new List<TestStepsMetadataJson>();
 
