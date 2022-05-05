cb83f8f912cf965aed1ae737f708fecaf99d14be
Navaneeth Thekkumpat <nthekkumpat@microsoft.com>
2022-05-05 16:23:18 -0400

    unittesting the scenario

--- src/PAModelTests/ReadTransformTests.cs
+++ src/PAModelTests/ReadTransformTests.cs
@@ -24,5 +24,22 @@ namespace PAModelTests
             msapp.ApplyAfterMsAppLoadTransforms(errorContainer);
             Assert.IsFalse(errorContainer.HasErrors);
         }
+
+        [DataTestMethod]
+        [DataRow("TestStepWithInvalidScreen.msapp")]
+        public void TestStepWithInvalidScreenAfterRead(string filename)
+        {
+            var path = Path.Combine(Environment.CurrentDirectory, "Apps", filename);
+            Assert.IsTrue(File.Exists(path));
+
+            (var msapp, var errorContainer) = CanvasDocument.LoadFromMsapp(path);
+            errorContainer.ThrowOnErrors();
+            msapp.ApplyAfterMsAppLoadTransforms(errorContainer);
+
+            // Test step with invalid screen is no more a error, but warning thrown
+            // Logic handled in AppTestsTransform.cs
+            Assert.IsFalse(errorContainer.HasErrors);
+            Assert.IsTrue(errorContainer.HasWarnings);
+        }
     }
 }
