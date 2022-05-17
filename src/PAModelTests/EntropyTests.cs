fc1636f3329af18d454cc7c8d07cb80a49702d29
Navaneeth Thekkumpat <nthekkumpat@microsoft.com>
2022-05-17 11:25:03 -0400

    unit testing the function parameter instances entropy values

---
+++ src/PAModelTests/EntropyTests.cs
@@ -0,0 +1,37 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.PowerPlatform.Formulas.Tools;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace PAModelTests
+{
+    [TestClass]
+    public class EntropyTests
+    {
+        [DataTestMethod]
+        [DataRow("ComponentTest.msapp")]
+        public void TestFunctionParameterOnInstances(string filename)
+        {
+            var root = Path.Combine(Environment.CurrentDirectory, "Apps", filename);
+            Assert.IsTrue(File.Exists(root));
+
+            (var msapp, var errors) = CanvasDocument.LoadFromMsapp(root);
+            errors.ThrowOnErrors();
+
+            Assert.IsNotNull(msapp._entropy.FunctionParamsInvariantScriptsonOnInstances);
+
+            using (var tempDir = new TempDir())
+            {
+                string outSrcDir = tempDir.Dir;
+
+                // Save to sources
+                ErrorContainer errorSources = msapp.SaveToSources(outSrcDir);
+                errors.ThrowOnErrors();
+            }
+        }
+    }
+}
