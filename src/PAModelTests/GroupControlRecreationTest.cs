7fbbb5e3b8caac8febb229dddd4274fd09fb1b0f
Navaneeth Thekkumpat <nthekkumpat@microsoft.com>
2023-02-23 16:14:31 -0500

    unittesting

--- src/PAModelTests/GroupControlRecreationTest.cs
+++ src/PAModelTests/GroupControlRecreationTest.cs
@@ -38,5 +38,40 @@ namespace PAModelTests
             // Check that the group control was still moved to the end of the children list
             Assert.AreEqual("Group1", sourceFile.Value.TopParent.Children.Last().Name);
         }
-    }
+        [DataTestMethod]
+        [DataRow("GroupControlTest.msapp")]
+        public void TestGroupControlsKey_Empty(string filename)
+        {
+            var path = Path.Combine(Environment.CurrentDirectory, "Apps", filename);
+            Assert.IsTrue(File.Exists(path));
+
+            // ApplyAfterMsAppLoadTransforms is called in LoadFromMsapp
+            (var msapp, var errorContainer) = CanvasDocument.LoadFromMsapp(path);
+            errorContainer.ThrowOnErrors();           
+
+            var errors = new ErrorContainer();
+            using (var tempDir = new TempDir())
+            {
+                string outSrcDir = tempDir.Dir;
+                if (msapp._screens.TryGetValue("Screen1", out var screen))
+                {
+                    screen.Children[0].Children.Clear();
+                }
+
+                // Save to sources
+                msapp.SaveToSources(outSrcDir);                
+
+                // Load app from the sources after deleting the entropy
+                var app = SourceSerializer.LoadFromSource(outSrcDir, errors);
+
+                using (var tempFile = new TempFile())
+                {
+                    errors = app.SaveToSources(outSrcDir, path);
+                }
+            }
+
+
+            Assert.IsTrue(errors.ToString().Contains("Warning PA2002: Validation issue: Group control state is empty for Group1"));
+        }
+    }    
 }
