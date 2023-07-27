a900312dfd41196c77ce8c548bf316bf113ff3b5
Landon Rogers <landonrogers@microsoft.com>
2023-07-27 17:01:59 -0400

    remove breaking ut change

--- src/PAModel/Serializers/TransformLogo.cs
+++ src/PAModel/Serializers/TransformLogo.cs
@@ -50,15 +50,13 @@ namespace Microsoft.PowerPlatform.Formulas.Tools
 
             if (!string.IsNullOrEmpty(publishInfo?.LogoFileName) && app._logoFile?.Name != null)
             {
-                if (app._assetFiles.Remove(app._logoFile.Name))
+                app._assetFiles.Remove(app._logoFile.Name);
+                publishInfo.LogoFileName = app._entropy.OldLogoFileName ?? Path.GetFileName(app._logoFile.Name.ToPlatformPath());
+                logoFile = new FileEntry
                 {
-                    publishInfo.LogoFileName = app._entropy.OldLogoFileName ?? Path.GetFileName(app._logoFile.Name.ToPlatformPath());
-                    logoFile = new FileEntry
-                    {
-                        Name = FilePath.RootedAt("Resources", FilePath.FromMsAppPath(publishInfo.LogoFileName)),
-                        RawBytes = app._logoFile.RawBytes
-                    };
-                }
+                    Name = FilePath.RootedAt("Resources", FilePath.FromMsAppPath(publishInfo.LogoFileName)),
+                    RawBytes = app._logoFile.RawBytes
+                };
             }
 
             return (publishInfo, logoFile);
