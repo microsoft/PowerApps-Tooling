7e4fc8b88e2cbfdec0a34632485e192eb1307167
David Guida <1432872+mizrael@users.noreply.github.com>
2024-01-24 13:52:56 -0500

    addressing PR comments

--- src/Microsoft.PowerPlatform.PowerApps.Persistence/Extensions/TypeExtensions.cs
+++ src/Microsoft.PowerPlatform.PowerApps.Persistence/Extensions/TypeExtensions.cs
@@ -1,10 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
 using Microsoft.PowerPlatform.PowerApps.Persistence.Models;
 
 namespace Microsoft.PowerPlatform.PowerApps.Persistence.Extensions;
