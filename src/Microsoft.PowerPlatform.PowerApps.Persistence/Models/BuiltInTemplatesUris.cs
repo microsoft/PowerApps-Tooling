f2d59bef4e1889e8ac1bb7fba256457c116602c8
David Guida <1432872+mizrael@users.noreply.github.com>
2024-01-24 16:40:14 -0500

    updated project signing config (#516)

--- src/Microsoft.PowerPlatform.PowerApps.Persistence/Models/BuiltInTemplatesUris.cs
+++ src/Microsoft.PowerPlatform.PowerApps.Persistence/Models/BuiltInTemplatesUris.cs
@@ -3,7 +3,7 @@
 
 namespace Microsoft.PowerPlatform.PowerApps.Persistence.Models;
 
-internal static class BuiltInTemplatesUris
+public static class BuiltInTemplatesUris
 {
     public const string Screen = "http://microsoft.com/appmagic/screen#screen";
     public const string Button = "http://microsoft.com/appmagic/button#button";
