cc7b2458cec993e282f5b11d124c28cde77f018b
David Guida <1432872+mizrael@users.noreply.github.com>
2024-01-29 11:52:26 -0500

    updated folder structure

--- src/Persistence/Json/JsonDoubleToIntConverter.cs
+++ src/Persistence/Json/JsonDoubleToIntConverter.cs
@@ -4,7 +4,7 @@
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
-namespace Microsoft.PowerPlatform.PowerApps.Persistence.JsonConverters;
+namespace Microsoft.PowerPlatform.PowerApps.Persistence.Json;
 
 public class JsonDoubleToIntConverter : JsonConverter<int>
 {
