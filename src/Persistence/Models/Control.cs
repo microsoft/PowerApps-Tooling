dea8c8ce323e8e13c761ef5e4fb3dba3a3dfa789
David Guida <1432872+mizrael@users.noreply.github.com>
2024-01-29 11:52:19 -0500

    removed references to YAML from Control

--- src/Persistence/Models/Control.cs
+++ src/Persistence/Models/Control.cs
@@ -4,15 +4,12 @@
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using Microsoft.PowerPlatform.PowerApps.Persistence.Collections;
-using YamlDotNet.Serialization;
 
 namespace Microsoft.PowerPlatform.PowerApps.Persistence.Models;
 
 [DebuggerDisplay("{Name}")]
 public abstract record Control
 {
-    public const string YamlName = "Control";
-
     public Control()
     {
     }
@@ -27,14 +24,12 @@ public abstract record Control
     /// <summary>
     /// template uri of the control.
     /// </summary>
-    [YamlMember(Alias = YamlName, Order = 0)]
     public required string ControlUri { get; init; }
 
     private readonly string _name = string.Empty;
     /// <summary>
     /// the control's name.
     /// </summary>
-    [YamlMember(Order = 1)]
     public required string Name
     {
         get => _name;
@@ -50,15 +45,12 @@ public abstract record Control
     /// <summary>
     /// key/value pairs of Control properties. Mapped to/from Control rules.
     /// </summary>
-    [YamlMember(Order = 2)]
     public ControlPropertiesCollection Properties { get; init; } = new();
 
     /// <summary>
     /// list of child controls nested under this control.
-    /// </summary>
-    [YamlMember(Order = 3)]
-    public IList<Control> Controls { get; init; } = new List<Control>();
+    /// </summary>    
+    public Control[] Controls { get; init; } = Array.Empty<Control>();
 
-    [YamlIgnore]
     public ControlEditorState? EditorState { get; set; }
 }
--- src/Persistence/Yaml/SerializerBuilderExtensions.cs
+++ src/Persistence/Yaml/SerializerBuilderExtensions.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using Microsoft.PowerPlatform.PowerApps.Persistence.Models;
 using YamlDotNet.Serialization;
 using YamlDotNet.Serialization.NamingConventions;
 
@@ -10,7 +11,7 @@ internal static class SerializerBuilderExtensions
 {
     public static SerializerBuilder WithFirstClassModels(this SerializerBuilder builder)
     {
-        builder = builder
+        builder = builder.AddAttributeOverrides()
             .WithEventEmitter(next => new FirstClassControlsEmitter(next))
             .WithNamingConvention(PascalCaseNamingConvention.Instance)
             .WithTypeInspector(inner => new ControlTypeInspector(inner))
@@ -23,7 +24,7 @@ internal static class SerializerBuilderExtensions
 
     public static DeserializerBuilder WithFirstClassModels(this DeserializerBuilder builder)
     {
-        return builder
+        return builder.AddAttributeOverrides()
             .WithNamingConvention(PascalCaseNamingConvention.Instance)
             .WithTypeInspector(inner => new ControlTypeInspector(inner))
             .WithTypeDiscriminatingNodeDeserializer(o =>
@@ -31,5 +32,22 @@ internal static class SerializerBuilderExtensions
                 o.AddTypeDiscriminator(new ControlTypeDiscriminator());
             })
             .WithTypeConverter(new ControlPropertiesCollectionConverter());
+    }
+
+    private static TBuilder AddAttributeOverrides<TBuilder>(this TBuilder builder)
+       where TBuilder : BuilderSkeleton<TBuilder>
+    {
+        var types = typeof(Control).Assembly.DefinedTypes;
+        foreach (var type in types)
+        {
+            builder = builder
+                .WithAttributeOverride(type, nameof(Control.ControlUri), new YamlMemberAttribute() { Order = 0, Alias = nameof(Control) })
+                .WithAttributeOverride(type, nameof(Control.Name), new YamlMemberAttribute() { Order = 1 })
+                .WithAttributeOverride(type, nameof(Control.Properties), new YamlMemberAttribute() { Order = 2 })
+                .WithAttributeOverride(type, nameof(Control.Controls), new YamlMemberAttribute() { Order = 3 })
+                .WithAttributeOverride(type, nameof(Control.EditorState), new YamlIgnoreAttribute());
+        }
+
+        return builder;
     }
 }
