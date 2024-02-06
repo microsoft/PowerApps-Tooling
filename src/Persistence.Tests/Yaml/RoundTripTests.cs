94c24ebc3de2fd1433fb1f3c08a1d0aacb9c201d
Andrew Petrochuk (from Dev Box) <anpetroc@microsoft.com>
2024-02-06 09:16:43 -0800

    Added tests for nested controls

--- src/Persistence.Tests/Yaml/RoundTripTests.cs
+++ src/Persistence.Tests/Yaml/RoundTripTests.cs
@@ -13,6 +13,10 @@ public class RoundTripTests : TestBase
     [TestMethod]
     [DataRow(@"_TestData/ValidYaml/Screen-with-name.fx.yaml", typeof(Screen), "http://microsoft.com/appmagic/screen", "My Power Apps Screen", 0, 0)]
     [DataRow(@"_TestData/ValidYaml/Screen-with-controls.fx.yaml", typeof(Screen), "http://microsoft.com/appmagic/screen", "Screen 1", 2, 2)]
+    [DataRow(@"_TestData/ValidYaml/Screen/with-two-properties.fx.yaml", typeof(Screen), "http://microsoft.com/appmagic/screen", "Hello", 2, 0)]
+    [DataRow(@"_TestData/ValidYaml/Screen/with-properties-and-controls.fx.yaml", typeof(Screen), "http://microsoft.com/appmagic/screen", "Screen with two properties and two controls", 2, 2)]
+    [DataRow(@"_TestData/ValidYaml/Screen/with-properties-and-nested-controls.fx.yaml", typeof(Screen), "http://microsoft.com/appmagic/screen", "Screen with two properties and two nested controls", 2, 2)]
+    [DataRow(@"_TestData/ValidYaml/Screen/with-multiline-properties.fx.yaml", typeof(Screen), "http://microsoft.com/appmagic/screen", "Screen with two multiline properties", 2, 0)]
     [DataRow(@"_TestData/ValidYaml/Control-with-custom-template.yaml", typeof(CustomControl), "http://localhost/#customcontrol", "My Power Apps Custom Control", 9, 0)]
     [DataRow(@"_TestData/ValidYaml/BuiltInControl1.yaml", typeof(BuiltInControl), "http://microsoft.com/appmagic/powercontrol/PowerApps_CoreControls_ButtonCanvas", "BuiltIn Control1", 1, 0)]
     public void RoundTrip_ValidYaml(string path, Type rootType, string expectedTemplateId, string expectedName, int expectedPropsCount, int expectedControlCount)
