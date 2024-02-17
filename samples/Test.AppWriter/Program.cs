bbcfeef325cb34c8a7f7e58f5dc543b5139a04bb
Andrew Petrochuk (from Dev Box) <anpetroc@microsoft.com>
2024-02-16 18:07:14 -0800

    Upated sample

--- samples/Test.AppWriter/Program.cs
+++ samples/Test.AppWriter/Program.cs
@@ -1,11 +1,11 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using Microsoft.PowerPlatform.PowerApps.Persistence.MsApp;
-using Microsoft.PowerPlatform.PowerApps.Persistence.Models;
-using Microsoft.PowerPlatform.PowerApps.Persistence.Templates;
-using Microsoft.PowerPlatform.PowerApps.Persistence.Extensions;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.PowerPlatform.PowerApps.Persistence.Extensions;
+using Microsoft.PowerPlatform.PowerApps.Persistence.Models;
+using Microsoft.PowerPlatform.PowerApps.Persistence.MsApp;
+using Microsoft.PowerPlatform.PowerApps.Persistence.Templates;
 
 namespace Test.AppWriter;
 
@@ -29,24 +29,50 @@ internal class Program
         var label1 = controlFactory.Create("Label1", template: "Label",
             properties: new()
             {
-                { "Text", @"""This app was created in .Net with YAML""" },
                 { "Align", "Align.Center" },
+                { "BorderColor", "RGBA(0, 18, 107, 1)" },
+                { "Color", "RGBA(0, 0, 0, 1)" },
+                { "DisabledColor", "RGBA(166, 166, 166, 1)" },
+                { "Height", "100" },
+                { "PaddingBottom", "20" },
+                { "PaddingTop", "20" },
+                { "Size", "26" },
+                { "ZIndex", "1" },
+                { "Text", @"""This app was created in .Net with YAML""" },
                 { "Width", "Parent.Width" }
             }
         );
         var button1 = controlFactory.Create("Button1", template: "Button",
             properties: new()
             {
+                { "DisabledBorderColor", "RGBA(166, 166, 166, 1)" },
+                { "DisabledColor", "RGBA(166, 166, 166, 1)" },
+                { "DisabledFill", "RGBA(244, 244, 244, 1)" },
+                { "Fill", "RGBA(56, 96, 178, 1)" },
+                { "FontWeight", "FontWeight.Semibold" },
+                { "HoverColor", "RGBA(255, 255, 255, 1)" },
+                { "HoverFill", "ColorFade(RGBA(56, 96, 178, 1), -20 %)" },
+                { "OnSelect", @"Notify(""Thank you"", NotificationType.Success)" },
+                { "Size", "15" },
                 { "Text", @"""Click me!""" },
-                { "Align", "Align.Center" },
-                { "Width", "Parent.Width" }
+                { "ZIndex", "2" },
             }
         );
 
         var groupContainer = controlFactory.Create("GroupContainer", template: "GroupContainer",
-            properties: new Dictionary<string, ControlPropertyValue>()
+            properties: new()
             {
-                { "Width", new() { Value = "App.Width" } }
+                { "DropShadow", "DropShadow.Light" },
+                { "LayoutAlignItems", "LayoutAlignItems.Center" },
+                { "LayoutDirection", "LayoutDirection.Vertical" },
+                { "LayoutJustifyContent", "LayoutJustifyContent.Center" },
+                { "LayoutMode", "LayoutMode.Auto" },
+                { "RadiusBottomLeft", "4" },
+                { "RadiusBottomRight", "4" },
+                { "RadiusTopLeft", "4" },
+                { "RadiusTopRight", "4" },
+                { "Width", "App.Width" },
+                { "ZIndex", "1" },
             },
             children: [label1, button1]
         );
