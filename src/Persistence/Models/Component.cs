3454733a268ec139e1e1a5faf1fcd417111740d4
David Guida <1432872+mizrael@users.noreply.github.com>
2024-03-05 14:16:32 +0100

    added support for Components (#571)

---
+++ src/Persistence/Models/Component.cs
@@ -0,0 +1,24 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.PowerPlatform.PowerApps.Persistence.Attributes;
+using Microsoft.PowerPlatform.PowerApps.Persistence.Templates;
+using YamlDotNet.Serialization;
+
+namespace Microsoft.PowerPlatform.PowerApps.Persistence.Models;
+
+[FirstClass(templateName: BuiltInTemplates.Component)]
+[YamlSerializable]
+public record Component : Control
+{
+    /// <summary>
+    /// Default constructor.
+    /// </summary>
+    [SetsRequiredMembers]
+    public Component(string name, IControlTemplateStore controlTemplateStore)
+    {
+        Name = name;
+        Template = controlTemplateStore.GetByName(BuiltInTemplates.Component);
+    }
+}
