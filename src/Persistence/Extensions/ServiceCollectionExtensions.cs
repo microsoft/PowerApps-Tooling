c0dc67e1124681b9da73c2fcde8377b32de20ff0
David Guida <1432872+mizrael@users.noreply.github.com>
2024-03-05 17:19:01 +0100

    updated DI configuration

--- src/Persistence/Extensions/ServiceCollectionExtensions.cs
+++ src/Persistence/Extensions/ServiceCollectionExtensions.cs
@@ -9,52 +9,46 @@ namespace Microsoft.PowerPlatform.PowerApps.Persistence.Extensions;
 
 public static class ServiceCollectionExtensions
 {
+    /// <summary>
+    /// registers the MSAPP persistence services
+    /// </summary>
+    /// <param name="services">the services collection instance.</param>
+    /// <param name="useDefaultTemplates">if true, registers the default templates (eg. 'text', 'button') on the templates store.</param>
     public static void AddPowerAppsPersistence(this IServiceCollection services, bool useDefaultTemplates = false)
     {
         services.AddSingleton<IMsappArchiveFactory, MsappArchiveFactory>();
         services.AddSingleton<IYamlSerializationFactory, YamlSerializationFactory>();
         services.AddSingleton<IControlFactory, ControlFactory>();
 
-        if (useDefaultTemplates)
-            services.AddSingleton<IControlTemplateStore, ControlTemplateStore>(WithDefaultTemplates);
-        else
-            services.AddSingleton<IControlTemplateStore, ControlTemplateStore>(WithMinimalTemplates);
+        services.AddSingleton<IControlTemplateStore, ControlTemplateStore>(ctx =>
+        {
+            var store = new ControlTemplateStore();
+
+            AddMinimalTemplates(store);
+
+            if (useDefaultTemplates)
+                AddDefaultTemplates(store);
+
+            store.DiscoverBuiltInTemplateTypes();
+
+            return store;
+        });
     }
 
-    /// <summary>
-    /// Adds default templates to the store.
-    /// </summary>
-    public static ControlTemplateStore WithDefaultTemplates(IServiceProvider _)
+    private static void AddMinimalTemplates(ControlTemplateStore store)
     {
-        var store = new ControlTemplateStore();
-
         store.Add(new ControlTemplate { Name = "hostControl", DisplayName = "host", Id = "http://microsoft.com/appmagic/hostcontrol" });
         store.Add(new ControlTemplate { Name = "appInfo", DisplayName = "app", Id = "http://microsoft.com/appmagic/appinfo" });
         store.Add(new ControlTemplate { Name = "screen", Id = "http://microsoft.com/appmagic/screen" });
-
         store.Add(new ControlTemplate { Name = "component", Id = "http://microsoft.com/appmagic/Component" });
+    }
 
+    private static void AddDefaultTemplates(ControlTemplateStore store)
+    {
         store.Add(new ControlTemplate { Name = "text", Id = "http://microsoft.com/appmagic/text" });
         store.Add(new ControlTemplate { Name = "button", Id = "http://microsoft.com/appmagic/button" });
 
         store.Add(new ControlTemplate { Name = "TextCanvas", Id = "http://microsoft.com/appmagic/powercontrol/PowerApps_CoreControls_TextCanvas" });
         store.Add(new ControlTemplate { Name = "ButtonCanvas", Id = "http://microsoft.com/appmagic/powercontrol/PowerApps_CoreControls_ButtonCanvas" });
-
-        store.DiscoverBuiltInTemplateTypes();
-
-        return store;
-    }
-
-    public static ControlTemplateStore WithMinimalTemplates(IServiceProvider _)
-    {
-        var store = new ControlTemplateStore();
-
-        store.Add(new ControlTemplate { Name = "hostControl", DisplayName = "host", Id = "http://microsoft.com/appmagic/hostcontrol" });
-        store.Add(new ControlTemplate { Name = "appInfo", DisplayName = "app", Id = "http://microsoft.com/appmagic/appinfo" });
-        store.Add(new ControlTemplate { Name = "screen", Id = "http://microsoft.com/appmagic/screen" });
-
-        store.DiscoverBuiltInTemplateTypes();
-
-        return store;
     }
 }
