e482011ac2ae3f1b883cb731a979dee9a8f5c8c4
cwduncan <92333534+cwduncan@users.noreply.github.com>
2024-03-06 14:47:14 -0800

    Add support for msapp generation commandline arguments (#573)

---
+++ samples/Test.AppWriter/ExampleAppGenerator.cs
@@ -0,0 +1,116 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.PowerPlatform.PowerApps.Persistence.Templates;
+using Microsoft.PowerPlatform.PowerApps.Persistence.Models;
+
+namespace Test.AppWriter;
+internal class ExampleAppGenerator
+{
+    public static List<(string, string)> ParseControlsInfo(string[] controlsinfo)
+    {
+        var tuples = new List<(string, string)>();
+
+        for (var i = 0; i < controlsinfo.Length; i += 2)
+        {
+            tuples.Add((controlsinfo[i], controlsinfo[i + 1]));
+        }
+
+        return tuples;
+    }
+
+    // This produces a simple example app including the specified number of screens and a few generic controls
+    // This is intended for testing purposes only
+    public static App GetExampleApp(IServiceProvider provider, string appName, int numScreens)
+    {
+        var controlFactory = provider.GetRequiredService<IControlFactory>();
+
+        var app = controlFactory.CreateApp(appName);
+
+        for (var i = 0; i < numScreens; i++)
+        {
+            var label1 = controlFactory.Create("Label1", template: "Label",
+                properties: new()
+                {
+                { "Align", "Align.Center" },
+                { "BorderColor", "RGBA(0, 18, 107, 1)" },
+                { "Color", "RGBA(0, 0, 0, 1)" },
+                { "DisabledColor", "RGBA(166, 166, 166, 1)" },
+                { "Height", "100" },
+                { "PaddingBottom", "20" },
+                { "PaddingTop", "20" },
+                { "Size", "26" },
+                { "ZIndex", "1" },
+                { "Text", @"""This app was created in .Net with YAML""" },
+                { "Width", "Parent.Width" }
+                }
+            );
+            var button1 = controlFactory.Create("Button1", template: "Button",
+                properties: new()
+                {
+                { "DisabledBorderColor", "RGBA(166, 166, 166, 1)" },
+                { "DisabledColor", "RGBA(166, 166, 166, 1)" },
+                { "DisabledFill", "RGBA(244, 244, 244, 1)" },
+                { "Fill", "RGBA(56, 96, 178, 1)" },
+                { "FontWeight", "FontWeight.Semibold" },
+                { "HoverColor", "RGBA(255, 255, 255, 1)" },
+                { "HoverFill", "ColorFade(RGBA(56, 96, 178, 1), -20 %)" },
+                { "OnSelect", @"Notify(""Thank you"", NotificationType.Success)" },
+                { "Size", "15" },
+                { "Text", @"""Click me!""" },
+                { "ZIndex", "2" },
+                }
+            );
+
+            var groupContainer = controlFactory.Create("GroupContainer", template: "GroupContainer",
+                properties: new()
+                {
+                { "DropShadow", "DropShadow.Light" },
+                { "LayoutAlignItems", "LayoutAlignItems.Center" },
+                { "LayoutDirection", "LayoutDirection.Vertical" },
+                { "LayoutJustifyContent", "LayoutJustifyContent.Center" },
+                { "LayoutMode", "LayoutMode.Auto" },
+                { "RadiusBottomLeft", "4" },
+                { "RadiusBottomRight", "4" },
+                { "RadiusTopLeft", "4" },
+                { "RadiusTopRight", "4" },
+                { "Width", "App.Width" },
+                { "ZIndex", "1" },
+                },
+                children: [label1, button1]
+            );
+
+            var graph = controlFactory.CreateScreen("Hello from .Net",
+                children: [groupContainer]
+            );
+
+            app.Screens.Add(graph);
+        }
+
+        return app;
+    }
+
+    // produce a specified app based on commandline input
+    public static App CreateApp(IServiceProvider provider, string appName, int numScreens, List<(string, string)> controls)
+    {
+        var controlFactory = provider.GetRequiredService<IControlFactory>();
+
+        var app = controlFactory.CreateApp(appName);
+
+        for (var i = 0; i < numScreens; i++)
+        {
+            var childlist = new List<Control>();
+            foreach (var control in controls)
+            {
+                childlist.Add(controlFactory.Create(control.Item1, template: control.Item2,
+                    properties: new()
+                ));
+            }
+
+            app.Screens.Add(controlFactory.CreateScreen("Hello from .Net", children: childlist.ToArray()));
+        }
+
+        return app;
+    }
+}
--- samples/Test.AppWriter/Program.cs
+++ samples/Test.AppWriter/Program.cs
@@ -1,11 +1,12 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System.Collections.Generic;
+using System.CommandLine;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.PowerPlatform.PowerApps.Persistence.Extensions;
 using Microsoft.PowerPlatform.PowerApps.Persistence.Models;
 using Microsoft.PowerPlatform.PowerApps.Persistence.MsApp;
-using Microsoft.PowerPlatform.PowerApps.Persistence.Templates;
 
 namespace Test.AppWriter;
 
@@ -20,99 +21,110 @@ internal class Program
         return serviceProvider;
     }
 
-    // This produces a simple example app including the specified number of screens and a few generic controls
-    // This is intended for testing purposes only
-    private static App GetExampleApp(IServiceProvider provider, string appName)
-    {
-        var controlFactory = provider.GetRequiredService<IControlFactory>();
-
-        var label1 = controlFactory.Create("Label1", template: "Label",
-            properties: new()
-            {
-                { "Align", "Align.Center" },
-                { "BorderColor", "RGBA(0, 18, 107, 1)" },
-                { "Color", "RGBA(0, 0, 0, 1)" },
-                { "DisabledColor", "RGBA(166, 166, 166, 1)" },
-                { "Height", "100" },
-                { "PaddingBottom", "20" },
-                { "PaddingTop", "20" },
-                { "Size", "26" },
-                { "ZIndex", "1" },
-                { "Text", @"""This app was created in .Net with YAML""" },
-                { "Width", "Parent.Width" }
-            }
-        );
-        var button1 = controlFactory.Create("Button1", template: "Button",
-            properties: new()
-            {
-                { "DisabledBorderColor", "RGBA(166, 166, 166, 1)" },
-                { "DisabledColor", "RGBA(166, 166, 166, 1)" },
-                { "DisabledFill", "RGBA(244, 244, 244, 1)" },
-                { "Fill", "RGBA(56, 96, 178, 1)" },
-                { "FontWeight", "FontWeight.Semibold" },
-                { "HoverColor", "RGBA(255, 255, 255, 1)" },
-                { "HoverFill", "ColorFade(RGBA(56, 96, 178, 1), -20 %)" },
-                { "OnSelect", @"Notify(""Thank you"", NotificationType.Success)" },
-                { "Size", "15" },
-                { "Text", @"""Click me!""" },
-                { "ZIndex", "2" },
-            }
-        );
-
-        var groupContainer = controlFactory.Create("GroupContainer", template: "GroupContainer",
-            properties: new()
-            {
-                { "DropShadow", "DropShadow.Light" },
-                { "LayoutAlignItems", "LayoutAlignItems.Center" },
-                { "LayoutDirection", "LayoutDirection.Vertical" },
-                { "LayoutJustifyContent", "LayoutJustifyContent.Center" },
-                { "LayoutMode", "LayoutMode.Auto" },
-                { "RadiusBottomLeft", "4" },
-                { "RadiusBottomRight", "4" },
-                { "RadiusTopLeft", "4" },
-                { "RadiusTopRight", "4" },
-                { "Width", "App.Width" },
-                { "ZIndex", "1" },
-            },
-            children: [label1, button1]
-        );
-
-        var graph = controlFactory.CreateScreen("Hello from .Net",
-            children: [groupContainer]
-        );
-        var app = controlFactory.CreateApp(appName);
-        app.Screens.Add(graph);
-
-        return app;
-    }
-
-    private static void Main(string[] args)
+    private static void CreateMSApp(string fullPathToMsApp, int numScreens)
     {
         // Setup services for creating MSApp representation
         var provider = ConfigureServiceProvider();
-
-        var fullPathToMsApp = args.Length > 0 ? args[0] : null;
-        if (fullPathToMsApp == null)
-        {
-            Console.WriteLine("No args provided, using default file path");
-            fullPathToMsApp = Path.Combine(Directory.GetCurrentDirectory(), "CanvasApp.msapp");
-            Console.WriteLine(fullPathToMsApp);
-        }
-
-        if (File.Exists(fullPathToMsApp)) // Overwrite
-        {
-            Console.WriteLine("Warning: File already exists;  Overwrite? (y / n)");
-            var input = Console.ReadLine();
-            if (input?.ToLower()[0] == 'y') File.Delete(fullPathToMsApp);
-        }
 
         // Create a new empty MSApp
         using var msapp = provider.GetRequiredService<IMsappArchiveFactory>().Create(fullPathToMsApp);
 
         // Add a basic example app (note: this will be replaced with interactive process)
-        msapp.App = GetExampleApp(provider, Path.GetFileNameWithoutExtension(fullPathToMsApp));
+        msapp.App = ExampleAppGenerator.GetExampleApp(provider, Path.GetFileNameWithoutExtension(fullPathToMsApp), numScreens);
 
         // Output the MSApp to the path provided
         msapp.Save();
+        Console.WriteLine("Success!  MSApp generated and saved to the provided path");
+    }
+
+    // Attempt to do specified app creation
+    private static void CreateMSApp(string fullPathToMsApp, int numScreens, string[] controlsinfo)
+    {
+        // Setup services for creating MSApp representation
+        var provider = ConfigureServiceProvider();
+
+        // Create a new empty MSApp
+        using var msapp = provider.GetRequiredService<IMsappArchiveFactory>().Create(fullPathToMsApp);
+
+        msapp.App = ExampleAppGenerator.CreateApp(provider, Path.GetFileNameWithoutExtension(fullPathToMsApp), numScreens, ExampleAppGenerator.ParseControlsInfo(controlsinfo));
+
+        // Output the MSApp to the path provided
+        msapp.Save();
+        Console.WriteLine("Success!  MSApp generated and saved to the provided path");
+    }
+
+    private static void Main(string[] args)
+    {
+        // This option will be enabled in the next PR
+        //var interactiveOption = new Option<bool>(
+        //    name: "--interactive",
+        //    description: "Enables interactive mode for MSApp creation"
+        //    );
+        var filePathOption = new Option<FileInfo?>(
+            name: "--filepath",
+            description: "(string) The path where the msapp file should be generated, including filename and extension",
+            parseArgument: result =>
+            {
+#pragma warning disable IDE0007 // Use implicit type
+                string? filePath = result.Tokens.Single().Value;
+#pragma warning restore IDE0007 // Use implicit type
+                if (File.Exists(filePath)) // TODO: Move this validation after input is handled
+                {
+                    Console.WriteLine("Warning: File already exists");
+                    Console.WriteLine("Provided path: " + filePath);
+                    Console.Write("    Overwrite? (y / n): ");
+                    var input = Console.ReadLine();
+                    if (input?.ToLower()[0] == 'y')
+                    {
+                        File.Delete(filePath);
+                        return new FileInfo(filePath);
+                    }
+                    else
+                    {
+                        result.ErrorMessage = "File already exists and overwrite declined, exiting";
+                        return null;
+                    }
+                }
+                else
+                {
+                    return new FileInfo(filePath);
+                }
+            }
+            )
+        { IsRequired = true };
+        var numScreensOption = new Option<int>(
+            name: "--numscreens",
+            description: "(integer) The number of screens to generate in the App",
+            getDefaultValue: () => 0
+            );
+        numScreensOption.AddValidator(result =>
+        {
+            if (result.GetValueForOption(numScreensOption) < 0)
+            {
+                result.ErrorMessage = "Number of screens must be greater than 0";
+            }
+        });
+        var controlsOptions = new Option<string[]>(
+            name: "--controls",
+            description: "(list of string) A list of control name and template pairs (i.e. mybutton Button labelname Label [controlname Template]...)")
+        { AllowMultipleArgumentsPerToken = true };
+
+
+        var rootCommand = new RootCommand("Test Writer for MSApp files.");
+        var createCommand = new Command("create", "Create a new MSApp at the specified path.")
+        {
+            filePathOption,
+            numScreensOption,
+            controlsOptions
+        };
+
+        rootCommand.AddCommand(createCommand);
+
+        createCommand.SetHandler((filepath, numscreens, controls) =>
+        {
+            CreateMSApp(filepath!.FullName, numscreens, controls);
+        }, filePathOption, numScreensOption, controlsOptions);
+
+        rootCommand.Invoke(args);
     }
 }
