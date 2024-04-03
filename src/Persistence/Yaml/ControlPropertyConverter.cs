b3d0c879d1d306b49e120ad0878514893bc075ee
Crash Collison <3751389+tehcrashxor@users.noreply.github.com>
2024-04-02 19:27:05 -0700

    Turn off TextFirst by default (#615)

--- src/Persistence/Yaml/ControlPropertyConverter.cs
+++ src/Persistence/Yaml/ControlPropertyConverter.cs
@@ -10,6 +10,8 @@ namespace Microsoft.PowerPlatform.PowerApps.Persistence.Yaml;
 
 internal class ControlPropertyConverter : IYamlTypeConverter
 {
+    static internal readonly char[] LineTerminators = new char[] { '\r', '\n', '\x85', '\x2028', '\x2029' };
+
     public bool Accepts(Type type)
     {
         return type == typeof(ControlProperty);
@@ -23,10 +25,31 @@ internal class ControlPropertyConverter : IYamlTypeConverter
     public void WriteYaml(IEmitter emitter, object? value, Type type)
     {
         var property = (ControlProperty)value!;
-        var style = property.IsFormula ? ScalarStyle.Literal : ScalarStyle.Plain;
+        var style = DetermineScalarStyleForProperty(property.Value);
 
 #pragma warning disable CS8604 // Possible null reference with property value, but it's legal in YAML.
         emitter.Emit(new Scalar(null, null, property.Value, style, true, false));
 #pragma warning restore CS8604 
+    }
+
+    internal static ScalarStyle DetermineScalarStyleForProperty(string? property)
+    {
+        if (property == null)
+        {
+            return ScalarStyle.Plain;
+        }
+        else if (property.Any(c => LineTerminators.Contains(c)))
+        {
+            return ScalarStyle.Literal;
+        }
+        else if (property.Contains(" #") || property.Contains(": "))
+        {
+            // These sequences break YAML parsing when outside of a literal block
+            return ScalarStyle.Literal;
+        }
+        else
+        {
+            return ScalarStyle.Plain;
+        }
     }
 }
