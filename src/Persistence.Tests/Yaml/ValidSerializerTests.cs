e2df2aeb2d51ba31ccfc7869e65c1dd1ce33d006
Shreya Shetty (from Dev Box) <shshetty@microsoft.com>
2024-06-13 15:24:53 -0700

    Skip ZIndex from being serialized.

--- src/Persistence.Tests/Yaml/ValidSerializerTests.cs
+++ src/Persistence.Tests/Yaml/ValidSerializerTests.cs
@@ -181,18 +181,56 @@ public class ValidSerializerTests : TestBase
                             properties: new()
                             {
                                 { "TemplateFill", "RGBA(0, 0, 0, 0)" },
+                                { "ZIndex", "1" },
                             }
                         ),
                         ControlFactory.Create("button12", template: "button",
                             properties: new()
                             {
                                 { "Fill", "RGBA(0, 0, 0, 0)" },
+                                { "ZIndex", "1" },
                             }
                         )
                     }
                 )
             }
         );
+
+        var serializer = CreateSerializer(isControlIdentifiers);
+
+        var sut = serializer.SerializeControl(graph).NormalizeNewlines();
+        var expectedYaml = File.ReadAllText(GetTestFilePath(expectedPath, isControlIdentifiers)).NormalizeNewlines();
+        sut.Should().Be(expectedYaml);
+    }
+
+    [TestMethod]
+    [DataRow(@"_TestData/ValidYaml{0}/ZIndexOrdering/with-addpropertiestoparents-control.pa.yaml", true)]
+    [DataRow(@"_TestData/ValidYaml{0}/ZIndexOrdering/with-addpropertiestoparents-control.pa.yaml", false)]
+    public void Serialize_Should_FlattenGalleryTemplateWithoutZIndex(string expectedPath, bool isControlIdentifiers)
+    {
+        var graph = ControlFactory.Create("Gallery1", template: "gallery",
+                   properties: new()
+                    {
+                        { "Items", "Accounts" },
+                    },
+                    children: new List<Control>()
+                    {
+                        ControlFactory.Create("GalleryTemplate1", template: "galleryTemplate",
+                            properties: new()
+                            {
+                                { "TemplateFill", "RGBA(0, 0, 0, 0)" },
+                                { "ZIndex", "1" },
+                            }
+                        ),
+                        ControlFactory.Create("Button1", template: "button",
+                            properties: new()
+                            {
+                                { "Fill", "RGBA(0, 0, 0, 0)" },
+                                { "ZIndex", "1" },
+                            }
+                        )
+                    }
+               );
 
         var serializer = CreateSerializer(isControlIdentifiers);
 
