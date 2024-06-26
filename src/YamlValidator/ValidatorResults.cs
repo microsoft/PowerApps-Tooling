e2aeb14888d775f71a3e3658f8838d77c1424a7c
Amruth Baskar <t-abaskar@microsoft.com>
2024-06-26 10:02:48 -0700

    change naming and make comment less verbose

--- src/YamlValidator/ValidatorResults.cs
+++ src/YamlValidator/ValidatorResults.cs
@@ -15,31 +15,24 @@ public class ValidatorResults
         TraversalResults = filterErrors(traversalResults);
     }
 
-    /* *
-     * Fix: this will filter out the false positives that are not relevant to the error output, when the validation is false
-     * Filters the errors to only include the errors that are relevant to the schema.
-     * For all instance paths in the schema with paths oneOf/0 and oneOf/1, we find the max suffix size and only keep the errors 
-     * from that OneOf path with the max suffix size.
-     * Why? since errors are propogated up the schema validation tree, the errors at the leaf nodes are the 
-     * most relevant but can have false positives due to the OneOf statement in the schema.
-     * */
+    //  This will filter out the false positives that are not relevant to the error output, when the validation is false
     private ReadOnlyCollection<ValidatorError> filterErrors(IReadOnlyList<ValidatorError> traversalResults)
     {
         var maxSchemaArraySuffixSize = 0;
         var maxSchemaObjectSuffixSize = 0;
-        var arrayTypeFile = "/oneOf/1";
-        var objectTypeFile = "/oneOf/0";
+        var arrayTypeSchemaPath = "/oneOf/1";
+        var objectTypeSchemaPath = "/oneOf/0";
         foreach (var err in traversalResults)
         {
             var errSchemaPath = err.SchemaPath;
-            if (!errSchemaPath.StartsWith(arrayTypeFile, StringComparison.Ordinal) &&
-                !err.SchemaPath.StartsWith(objectTypeFile, StringComparison.Ordinal))
+            if (!errSchemaPath.StartsWith(arrayTypeSchemaPath, StringComparison.Ordinal) &&
+                !err.SchemaPath.StartsWith(objectTypeSchemaPath, StringComparison.Ordinal))
             {
                 continue;
             }
 
-            var suffixLength = errSchemaPath.Length - arrayTypeFile.Length;
-            if (errSchemaPath.StartsWith(arrayTypeFile, StringComparison.Ordinal))
+            var suffixLength = errSchemaPath.Length - arrayTypeSchemaPath.Length;
+            if (errSchemaPath.StartsWith(arrayTypeSchemaPath, StringComparison.Ordinal))
             {
                 maxSchemaArraySuffixSize = Math.Max(maxSchemaArraySuffixSize, suffixLength);
             }
@@ -52,14 +45,14 @@ public class ValidatorResults
         foreach (var err in traversalResults)
         {
             var errSchemaPath = err.SchemaPath;
-            if (!errSchemaPath.StartsWith(arrayTypeFile, StringComparison.Ordinal) &&
-                !err.SchemaPath.StartsWith(objectTypeFile, StringComparison.Ordinal))
+            if (!errSchemaPath.StartsWith(arrayTypeSchemaPath, StringComparison.Ordinal) &&
+                !err.SchemaPath.StartsWith(objectTypeSchemaPath, StringComparison.Ordinal))
             {
                 filteredErrors.Add(err);
                 continue;
             }
 
-            if (errSchemaPath.StartsWith(arrayTypeFile, StringComparison.Ordinal))
+            if (errSchemaPath.StartsWith(arrayTypeSchemaPath, StringComparison.Ordinal))
             {
                 if (maxSchemaArraySuffixSize >= maxSchemaObjectSuffixSize)
                 {
