7e3ff87c4288c042442c437e15d9278647e475f8
Amruth Baskar <t-abaskar@microsoft.com>
2024-07-03 10:58:25 -0700

    relative filepath issue (maybe?)

--- scripts/PersistenceValidate.sh
+++ scripts/PersistenceValidate.sh
@@ -2,6 +2,7 @@
 
 # This script is used to validate whether persistence has valid tests
 
+echo $(pwd)
 cd ../bin/Debug/YamlValidator
 
 appTestResults=$(dotnet YamlValidator.dll validate --path "../Persistence.Tests/_TestData/AppsWithYaml")
