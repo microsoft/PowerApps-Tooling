f3eeb113f790df66ed9887f729de34735e1764a5
Amruth Baskar <t-abaskar@microsoft.com>
2024-07-03 11:02:56 -0700

    more debugging

--- scripts/PersistenceValidate.sh
+++ scripts/PersistenceValidate.sh
@@ -3,6 +3,10 @@
 # This script is used to validate whether persistence has valid tests
 
 echo $(pwd)
+cd ..
+echo $(ls)
+cd ./bin
+echo $(ls)
 cd ../bin/Debug/YamlValidator
 
 appTestResults=$(dotnet YamlValidator.dll validate --path "../Persistence.Tests/_TestData/AppsWithYaml")
