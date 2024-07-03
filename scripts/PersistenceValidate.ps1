90666f81c818e51274f760337e645200631ff9fc
Amruth Baskar <t-abaskar@microsoft.com>
2024-07-03 16:06:59 -0700

    run full updated CI workflow

--- scripts/PersistenceValidate.ps1
+++ scripts/PersistenceValidate.ps1
@@ -3,10 +3,21 @@
   Uses the repository's root as entry point (similar to how containers on github actions would)
 #>
 
+$AppTestDir = "..\Persistence.Tests\_TestData\AppsWithYaml"
+$ControlTestDir = "..\Persistence.Tests\_TestData\ValidYaml-CI"
+
+# come back to same directory after validation
+Push-Location
+
 Set-Location -Path "bin\Debug\YamlValidator"
 
-$appTestResults = dotnet YamlValidator.dll validate --path "..\Persistence.Tests\_TestData\AppsWithYaml"
-$appTestResults
+$AppTestResults = dotnet YamlValidator.dll validate --path $AppTestDir
+Write-Output "Validating Directory $AppTestDir `n"
+$AppTestResults
 
-$controlTestResults = dotnet YamlValidator.dll validate --path "..\Persistence.Tests\_TestData\ValidYaml-CI"
-$controlTestResults
+$ControlTestResults = dotnet YamlValidator.dll validate --path $ControlTestDir
+Write-Output "Validating Directory $ControlTestDir `n"
+$ControlTestResults
+
+# restore location
+Pop-Location
--- scripts/PersistenceValidate.sh
+++ scripts/PersistenceValidate.sh
@@ -4,8 +4,13 @@
 
 cd bin/Debug/YamlValidator
 
-appTestResults=$(dotnet YamlValidator.dll validate --path "../Persistence.Tests/_TestData/AppsWithYaml")
-echo "$appTestResults"
+APP_TEST_DIR="../Persistence.Tests/_TestData/AppsWithYaml"
+CONTROL_TEST_DIR="../Persistence.Tests/_TestData/ValidYaml-CI"
 
-controlTestResults=$(dotnet YamlValidator.dll validate --path "../Persistence.Tests/_TestData/ValidYaml-CI")
-echo "$controlTestResults"
+app_test_results=$(dotnet YamlValidator.dll validate --path $APP_TEST_DIR)
+printf "Validating Directory $APP_TEST_DIR \n"
+printf "$app_test_results"
+
+control_test_results=$(dotnet YamlValidator.dll validate --path $CONTROL_TEST_DIR)
+printf "Validating Directory $CONTROL_TEST_DIR \n"
+printf "$control_test_results"
