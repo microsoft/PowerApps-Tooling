53227526ce3118f50052c8eb6e35af33d4423e0d
Amruth Baskar <t-abaskar@microsoft.com>
2024-07-16 17:03:05 -0700

    rewrite of schema loader to store schema in dll on build and access from there

--- src/Persistence/YamlValidator/SchemaLoader.cs
+++ src/Persistence/YamlValidator/SchemaLoader.cs
@@ -1,28 +1,46 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
+using System.Reflection;
 using Json.Schema;
 
 namespace Microsoft.PowerPlatform.PowerApps.Persistence.YamlValidator;
 
 public class SchemaLoader
 {
+    private const string _schemaFolderPath = "schema";
     private const string _subschemaFolderPath = "subschemas";
-    private static readonly string _schemaPath = Path.Combine(".", "YamlValidator", "schema", "pa.yaml-schema.json");
 
     public JsonSchema Load()
     {
-        var node = JsonSchema.FromFile(_schemaPath);
-        var schemaFolder = Path.GetDirectoryName(_schemaPath);
-        var subschemaPaths = Directory.GetFiles($@"{schemaFolder}{Path.DirectorySeparatorChar}{_subschemaFolderPath}",
-            $"*{Constants.JsonFileExtension}");
-
-        foreach (var path in subschemaPaths)
+        var assembly = Assembly.GetExecutingAssembly();
+        JsonSchema? node = null;
+        foreach (var file in assembly.GetManifestResourceNames())
         {
-            var subschema = JsonSchema.FromFile(path);
-            SchemaRegistry.Global.Register(subschema);
-        }
+            var fileStream = assembly.GetManifestResourceStream(file);
+            var assemblyName = Assembly.GetExecutingAssembly().GetName().Name;
+            if (fileStream == null)
+            {
+                throw new IOException($"Resource {file} could not found in assembly {assemblyName}");
+            }
+            using var streamReader = new StreamReader(fileStream);
+            var jsonSchemaString = streamReader.ReadToEnd();
+            var schema = JsonSchema.FromText(jsonSchemaString);
+            if (file.StartsWith($"{assemblyName}.{_subschemaFolderPath}", StringComparison.Ordinal))
+            {
+                var subschemaNameLength = $"{assemblyName}.{_subschemaFolderPath}.".Length;
+                var subschemaName = file.Substring(subschemaNameLength);
+                schema.BaseUri = new Uri($"file://{_schemaFolderPath}/{_subschemaFolderPath}/");
+                SchemaRegistry.Global.Register(schema);
+                continue;
 
+            }
+            var schemaNameLength = $"{assemblyName}.".Length;
+            var schemaName = file.Substring(schemaNameLength);
+            schema.BaseUri = new Uri($"file://{_schemaFolderPath}");
+            node = schema;
+        }
+        if (node == null)
+        {
+            throw new InvalidDataException("Schema was not able to be read into memory");
+        }
         return node;
     }
 }
