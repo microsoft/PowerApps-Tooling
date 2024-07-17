378656ed143905be4201211ccbc45c61a22954ca
Amruth Baskar <t-abaskar@microsoft.com>
2024-07-17 12:42:49 -0700

    keep current commit head

--- src/Persistence/YamlValidator/Constants.cs
+++ src/Persistence/YamlValidator/Constants.cs
@@ -5,7 +5,6 @@ namespace Microsoft.PowerPlatform.PowerApps.Persistence.YamlValidator;
 
 public static class Constants
 {
-<<<<<<< HEAD
     public const string YamlFileExtension = ".pa.yaml";
     public const string JsonFileExtension = ".json";
 
@@ -13,16 +12,4 @@ public static class Constants
     public const string emptyYamlError = "Empty YAML file";
 
     public const string subNamespace = "YamlValidator";
-=======
-    public const string FileTypeName = "file";
-    public const string FolderTypeName = "folder";
-    public const string YamlFileExtension = ".pa.yaml";
-    public const string JsonFileExtension = ".json";
-
-    public const string Verbose = "verbose";
-
-    // runtime constants
-    // default schema path
-    public static readonly string DefaultSchemaPath = Path.Combine(".", "schema", "pa.yaml-schema.json");
->>>>>>> master
 }
