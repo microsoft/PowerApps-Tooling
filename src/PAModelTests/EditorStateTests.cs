7f35931286623008e1f4c2e5e51f22954e4c9eaf
Youssef1313 <youssefvictor00@gmail.com>
2024-11-27 21:15:01 +0100

    Fix build

--- src/PAModelTests/EditorStateTests.cs
+++ src/PAModelTests/EditorStateTests.cs
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using System.IO;
 using Microsoft.PowerPlatform.Formulas.Tools;
 using Microsoft.PowerPlatform.Formulas.Tools.EditorState;
 using Microsoft.PowerPlatform.Formulas.Tools.IO;
