c7b5584bb7def408b7fe89afcaa9ea63e7d34ce1
Jame Vaalet <jamevaalet@outlook.com>
2025-03-27 16:46:29 -0700

    update type to string[]

--- src/Persistence/PaYaml/Models/SchemaV3/EditorStateInstance.cs
+++ src/Persistence/PaYaml/Models/SchemaV3/EditorStateInstance.cs
@@ -1,13 +1,11 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using System.Collections.Immutable;
-
 namespace Microsoft.PowerPlatform.PowerApps.Persistence.PaYaml.Models.SchemaV3;
 
 public record EditorStateInstance
 {
-    public ImmutableArray<string>? ScreensOrder { get; init; }
+    public string[]? ScreensOrder { get; init; }
 
-    public ImmutableArray<string>? ComponentDefinitionsOrder { get; init; }
+    public string[]? ComponentDefinitionsOrder { get; init; }
 }
