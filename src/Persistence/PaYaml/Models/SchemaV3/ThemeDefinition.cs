910cc863becc26113353f6f7577b55ab24ce8057
Crash Collison <3751389+tehcrashxor@users.noreply.github.com>
2025-10-20 14:54:09 -0700

    Initial theme support

---
+++ src/Persistence/PaYaml/Models/SchemaV3/ThemeDefinition.cs
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace Microsoft.PowerPlatform.PowerApps.Persistence.PaYaml.Models.SchemaV3;
+
+public record ThemeDefinition
+{
+    public required string BasePaletteColor { get; init; }
+    public int? HueTorsion { get; init; }
+    public int? Vibrancy { get; init; }
+    public bool ThemePrimaryColorLocked { get; init; }
+    public string[]? ColorOverrides { get; init; }
+}
