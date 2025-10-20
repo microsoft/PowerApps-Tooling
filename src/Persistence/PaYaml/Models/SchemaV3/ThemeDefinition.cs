aa3d96a9120ae6222fceaf9ce8c016a4358a42d1
Crash Collison <3751389+tehcrashxor@users.noreply.github.com>
2025-10-20 15:52:14 -0700

    Named mapping for color overrides following current UI naming scheme

--- src/Persistence/PaYaml/Models/SchemaV3/ThemeDefinition.cs
+++ src/Persistence/PaYaml/Models/SchemaV3/ThemeDefinition.cs
@@ -9,6 +9,26 @@ public record ThemeDefinition
     public int? HueTorsion { get; init; }
     public int? Vibrancy { get; init; }
     public bool? ThemePrimaryColorLocked { get; init; }
-    public string[]? ColorOverrides { get; init; }
+    public ColorOverrides? ColorOverrides { get; init; }
     public string? Font { get; init; }
+}
+
+public record ColorOverrides
+{
+    public string? Darker70 { get; init; }
+    public string? Darker60 { get; init; }
+    public string? Darker50 { get; init; }
+    public string? Darker40 { get; init; }
+    public string? Darker30 { get; init; }
+    public string? Darker20 { get; init; }
+    public string? Darker10 { get; init; }
+    public string? PrimaryColor { get; init; }
+    public string? Lighter10 { get; init; }
+    public string? Lighter20 { get; init; }
+    public string? Lighter30 { get; init; }
+    public string? Lighter40 { get; init; }
+    public string? Lighter50 { get; init; }
+    public string? Lighter60 { get; init; }
+    public string? Lighter70 { get; init; }
+    public string? Lighter80 { get; init; }
 }
