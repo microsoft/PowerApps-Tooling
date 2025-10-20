996994c53f47b3f764da8e8835c0abfecf643c13
Crash Collison <3751389+tehcrashxor@users.noreply.github.com>
2025-10-20 15:45:56 -0700

    PR feedback - nullable types

--- src/Persistence/PaYaml/Models/SchemaV3/ThemeDefinition.cs
+++ src/Persistence/PaYaml/Models/SchemaV3/ThemeDefinition.cs
@@ -5,10 +5,10 @@ namespace Microsoft.PowerPlatform.PowerApps.Persistence.PaYaml.Models.SchemaV3;
 
 public record ThemeDefinition
 {
-    public required string BasePaletteColor { get; init; }
+    public required string? BasePaletteColor { get; init; }
     public int? HueTorsion { get; init; }
     public int? Vibrancy { get; init; }
-    public bool ThemePrimaryColorLocked { get; init; }
+    public bool? ThemePrimaryColorLocked { get; init; }
     public string[]? ColorOverrides { get; init; }
     public string? Font { get; init; }
 }
