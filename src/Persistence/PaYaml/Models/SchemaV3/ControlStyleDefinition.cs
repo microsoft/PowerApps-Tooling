d1a2e370dac52190de2771684e208135566ab1b1
Crash Collison <3751389+tehcrashxor@users.noreply.github.com>
2026-06-24 15:50:46 -0700

    Remove unused using in ControlStyleDefinition

--- src/Persistence/PaYaml/Models/SchemaV3/ControlStyleDefinition.cs
+++ src/Persistence/PaYaml/Models/SchemaV3/ControlStyleDefinition.cs
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using System.Diagnostics.CodeAnalysis;
 using Microsoft.PowerPlatform.PowerApps.Persistence.PaYaml.Models.PowerFx;
 using YamlDotNet.Serialization;
 
